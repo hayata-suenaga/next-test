1fad8d58bb08f5d4acef38b9918999f7744c5e12
Hayata Suenaga <hayata@expensify.com>
2023-10-15 22:34:11 +0900

    chore: adjust menu items

--- src/components/Shell/consts.ts
+++ src/components/Shell/consts.ts
@@ -5,20 +5,22 @@ import {
   CurrencyDollarIcon,
   AdjustmentsVerticalIcon,
   UserGroupIcon,
+  CheckBadgeIcon,
 } from "@heroicons/react/24/outline";
 
 export const mainMenuItems = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
+  { name: "Todos", href: "#", icon: CheckBadgeIcon, current: false },
+  { name: "Insights", href: "#", icon: ChartPieIcon, current: false },
+  { name: "Budgets", href: "#", icon: AdjustmentsVerticalIcon, current: false },
   { name: "Reports", href: "#", icon: ReceiptPercentIcon, current: false },
   { name: "Expenses", href: "#", icon: CurrencyDollarIcon, current: false },
-  { name: "Budgets", href: "#", icon: AdjustmentsVerticalIcon, current: false },
-  { name: "Insights", href: "#", icon: ChartPieIcon, current: false },
   { name: "Policies", href: "#", icon: UserGroupIcon, current: false },
 ];
 export const teamMenuItems = [
-  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
-  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
+  { id: 1, name: "Product", href: "#", initial: "P", current: false },
+  { id: 2, name: "Design", href: "#", initial: "D", current: false },
+  { id: 3, name: "Marketing", href: "#", initial: "M", current: false },
 ];
 export const userMenuItems = [
   { name: "Your profile", href: "#" },
