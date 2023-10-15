fe51caaf722d11fd76e47c314759eb78bcabd9cd
Hayata Suenaga <hayata@expensify.com>
2023-10-15 22:00:43 +0900

    feat: fetch reports for the user

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,3 +1,14 @@
-export default function Reports() {
+import prisma from "@/db";
+
+async function getAllReports(userId: string) {
+  return await prisma.report.findMany({
+    where: {
+      authorId: userId,
+    },
+  });
+}
+
+export default async function Reports(userId: string) {
+  const reports = await getAllReports(userId);
   return <h1>Hello</h1>;
 }
