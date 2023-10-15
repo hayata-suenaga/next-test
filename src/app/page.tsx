f5e70f31a976d3da23737dcf6a36d2bc7c42c4ce
Hayata Suenaga <hayata@expensify.com>
2023-10-15 10:12:31 +0900

    feat: add New page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Hello, Next.js</h1>;
+  return (
+    <>
+      <header className="flex justify-between mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          href="/new"
+          className="border border-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+        >
+          New
+        </Link>
+      </header>
+      <ul></ul>
+    </>
+  );
 }
