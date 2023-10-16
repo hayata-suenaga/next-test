dbda4491d26a88b8e93ca6124d298d14b0c29793
Hayata Suenaga <hayata@expensify.com>
2023-10-16 16:47:51 +0900

    feat: provide session to client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import Shell from "@/components/Shell";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { getServerSession } from "next-auth";
+import SessionProvider from "@/components/SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,15 +11,19 @@ export const metadata: Metadata = {
   title: "ToDo App",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
+
   return (
     <html lang="en" className="h-full bg-white">
       <body className={`${inter.className} bg-white`}>
-        <Shell>{children}</Shell>
+        <SessionProvider session={session}>
+          <Shell>{children}</Shell>
+        </SessionProvider>
       </body>
     </html>
   );
