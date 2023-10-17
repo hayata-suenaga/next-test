1b7bfcee0ae874fc60a098733f444379b7c14ab7
Hayata Suenaga <hayata@expensify.com>
2023-10-17 13:05:16 +0900

    chore: remove unnecessary root layout style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full bg-white">
-      <body className={`${inter.className} bg-white`}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={`${inter.className} h-full`}>{children}</body>
     </html>
   );
 }
